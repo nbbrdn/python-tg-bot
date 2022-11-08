8de1047971e0f137840cbcf6f08c8515126f97bd
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-11-08 22:28:25 +1000

    Implement the inline keyboard

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import TOKEN
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
+
+ikb = InlineKeyboardMarkup(row_width=2)
+ib1 = InlineKeyboardButton(text='Button 1', url='https://youtube.com/')
+ib2 = InlineKeyboardButton(text='Button 2', url='https://github.com/')
+ikb.add(ib1, ib2)
+
+
+@dp.message_handler(commands=['start'])
+async def send_kb(message: types.Message):
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text='Hello World!',
+        reply_markup=ikb,
+    )
 
 
 @dp.message_handler()
